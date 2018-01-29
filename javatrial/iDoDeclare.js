8931da7e37edd21980cfa6872afa4c8071c1be5e
Vanessa Esterbrook <vesterbrook@Vanessas-MacBook-Pro-2.local>
2018-01-29 12:22:48 -0600

    Practice makes perfect

---
+++ javatrial/iDoDeclare.js
@@ -0,0 +1,22 @@
+var num1 =-42;
+var num2 =23;
+var num3 =14;
+var num4 =0;
+
+var firstName = "Laru";
+var lastName = "London";
+var random = "bridges";
+
+var moonIsNight = 25;
+var sunIsDark = 30;
+
+console.log(num1)
+console.log(num2)
+console.log(num3)
+console.log(num4)
+console.log("The First string", firstName)
+console.log ("The Second String", lastName)
+console.log("The Third String", random)
+console.log(moonIsNight<30)
+console.log(sunIsDark<25)
+console.log(i)
---
+++ javatrial/java.js
@@ -0,0 +1,6 @@
+var HOUR = 8;
+var MINUTE = 50;
+var PERIOD = "AM";
+
+console.log( "it's almost 9 in the morning")
+
